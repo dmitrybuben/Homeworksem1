bdb3bbdd027f1530cf26dc040234c381055e192e
dmitrybuben <dmitry.buben@gmail.com>
2022-02-04 22:05:28 +0300

    task6

---
+++ task6/Program.cs
@@ -0,0 +1,13 @@
+﻿// Выяснить является ли число чётным
+Console.Write("Введите целое число: ");
+
+int num = int.Parse(Console.ReadLine());
+
+if(num % 2 == 0)
+{
+    Console.WriteLine("Число является четным");
+}
+else
+{
+    Console.WriteLine("Число не является четным");
+}
