e21e3944025f76ee7c7fe3cdd5911ec6e3a9be88
dmitrybuben <dmitry.buben@gmail.com>
2022-02-04 22:05:41 +0300

    task8

---
+++ task8/Program.cs
@@ -0,0 +1,16 @@
+﻿// Показать четные числа от 1 до N
+int N = new Random().Next(1, 20);
+Console.WriteLine("Случайное число N: " + N);
+
+int num = 2;
+
+if(N < num) Console.WriteLine("Четных чисел нет");
+
+Console.WriteLine("Четные числа:");
+
+while(num < N)
+{
+    Console.WriteLine(num);
+    num = num + 2;
+}
+if(num == N) Console.WriteLine(num);
