69e8e2e0e7de21027a68ca3a811d951b3f62b6b0
dmitrybuben <dmitry.buben@gmail.com>
2022-02-09 17:59:13 +0300

    task12

---
+++ task12/Program.cs
@@ -0,0 +1,6 @@
+﻿// Удалить вторую цифру трехзначного числа.
+int number = new Random().Next(100, 1000);
+Console.WriteLine("Случайное трехзначное число: " + number);
+int first = number / 100;
+int third = (number % 10);
+Console.WriteLine("Случайное число без второй цифры: " + first + third);
