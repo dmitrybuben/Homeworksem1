ced323a1e8d4f6edc7f12d97817e9e742f2af7f5
dmitrybuben <dmitry.buben@gmail.com>
2022-02-09 17:58:43 +0300

    task10

---
+++ task10/Program.cs
@@ -0,0 +1,4 @@
+﻿// Показать вторую цифру трёхзначного числа
+Console.Write("Введите трехзначное число: ");
+int num = int.Parse(Console.ReadLine());
+Console.WriteLine("Вторая цифра: " + (num % 100) / 10);
