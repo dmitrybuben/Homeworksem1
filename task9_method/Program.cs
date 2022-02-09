2f7cfe9057b9edd7b8c3f007bca92dcd26de7765
dmitrybuben <dmitry.buben@gmail.com>
2022-02-09 18:00:47 +0300

    task9_method

---
+++ task9_method/Program.cs
@@ -0,0 +1,16 @@
+﻿// Показать последнюю цифру трехзначного числа
+/*Console.WriteLine("Введите трехзначное число: ");
+int num = int.Parse(Console.ReadLine());
+num = num % 10;
+Console.WriteLine("Последняя цифра числа: " + num);
+*/
+
+// Показать последнюю цифру трехзначного числа
+
+int LastNumber(int arg)
+{
+    return arg % 10;
+}
+Console.Write("Введите трехзначное число: ");
+int number = int.Parse(Console.ReadLine());
+Console.WriteLine("Последняя цифра введенного числа: " + LastNumber(number));
