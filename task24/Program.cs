73f7f1a4d09a79be22830ad1728bfb527c5e75b2
dmitrybuben <dmitry.buben@gmail.com>
2022-02-13 18:14:32 +0300

    task24

---
+++ task24/Program.cs
@@ -0,0 +1,7 @@
+﻿// Найти кубы чисел от 1 до N
+int N = new Random().Next(1, 10);
+Console.WriteLine("N: " + N);
+for(int i = 1; i < N+1; i ++)
+{
+    Console.WriteLine($"Куб числа {i}: {i*i*i}");
+}
