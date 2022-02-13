1428c17542ba7707a889521eb489bc9f91b85c23
dmitrybuben <dmitry.buben@gmail.com>
2022-02-13 18:13:58 +0300

    task20

---
+++ task20/Program.cs
@@ -0,0 +1,46 @@
+﻿// Задача 20: Задать номер четверти, показать диапазоны для возможных координат.
+int Range(int num)
+{
+if (num == 1)
+{
+    Console.WriteLine("Диапазоны возможных координат: x > 0, y > 0");
+} 
+else if(num == 2)
+{
+    Console.WriteLine("Диапазоны возможных координат: x < 0, y > 0");
+}
+else if(num == 3)
+{
+    Console.WriteLine("Диапазоны возможных координат: x < 0, y < 0");
+}
+else if(num == 4)
+{
+    Console.WriteLine("Диапазоны возможных координат: x > 0, y < 0");
+}
+return num;
+}
+
+int qternumber = new Random().Next(1, 4);
+Console.WriteLine("Номер четверти: " + Range(qternumber));
+
+
+/*int qternumber = new Random().Next(1, 4);
+Console.WriteLine("Номер четверти: " + qternumber);
+
+if (qternumber == 1)
+{
+    Console.WriteLine("Диапазоны возможных координат: x > 0, y > 0");
+} 
+else if(qternumber == 2)
+{
+    Console.WriteLine("Диапазоны возможных координат: x < 0, y > 0");
+}
+else if(qternumber == 3)
+{
+    Console.WriteLine("Диапазоны возможных координат: x < 0, y < 0");
+}
+else if(qternumber == 4)
+{
+    Console.WriteLine("Диапазоны возможных координат: x > 0, y < 0");
+}
+*/
