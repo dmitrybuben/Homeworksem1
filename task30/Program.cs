941243dec825fd775fc71559c8d880bc149c0ab8
dmitrybuben <dmitry.buben@gmail.com>
2022-02-15 11:52:50 +0300

    task30

---
+++ task30/Program.cs
@@ -0,0 +1,8 @@
+﻿// Покажите кубы чисел, заканчивающихся на чётную цифру.
+int N = new Random().Next(1, 36);
+Console.WriteLine("Граница интервала: " + N);
+for (double i = 0; i <= N; i=i+2)
+{
+    Console.WriteLine($"Куб числа {i} равен {i*i*i}");
+}
+//i*i*i
