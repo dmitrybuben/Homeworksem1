cfde29ab3b9891a0273f4568c6218b52cdf87d35
dmitrybuben <dmitry.buben@gmail.com>
2022-02-15 10:40:46 +0300

    task28

---
+++ task28/Program.cs
@@ -0,0 +1,22 @@
+﻿// Подсчитайте сумму цифр в числе.
+int summ(int n)
+{
+    int s = 0;
+    if(n == 0)
+    {
+        return s;
+    }
+    else if (n < 0)
+    {
+        n = n * (-1);
+    }
+    while (n > 0)
+    {
+        s = s + n % 10;
+        n = n / 10;
+    }
+    return s;
+}
+int N = new Random().Next(-1000, 1000);
+Console.WriteLine("Случайное число: " + N);
+Console.WriteLine("Сумма цифр в числе: " + summ(N));
