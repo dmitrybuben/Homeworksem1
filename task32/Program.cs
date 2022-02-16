9e2d7b7b467d1447787748daf7c8b477d4715dc1
dmitrybuben <dmitry.buben@gmail.com>
2022-02-16 10:55:43 +0300

    task32

---
+++ task32/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задайте массив из восьми элементов, заполненных нулями и единицами. Выведите их на экран.
+
+void FillArray(int[]array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(0,2);
+    }
+}
+void PrintArray(int[]collection)
+{
+    for (int i = 0; i < collection.Length; i++)
+    {
+        Console.Write($"{collection[i]} ");   
+    }
+    Console.WriteLine();
+}
+int[]arr = new int[10];
+FillArray(arr);
+PrintArray(arr);
