392aee126ee21bc0a961e14d107599a52df52137
dmitrybuben <dmitry.buben@gmail.com>
2022-02-16 11:32:04 +0300

    task26

--- task26/Program.cs
+++ task26/Program.cs
@@ -1,31 +1,11 @@
 ﻿// Возведите число А в натуральную степень B, используя цикл.
 
-// Решение 1.
-/*int A = new Random().Next(-10, 10);
-Console.WriteLine("Случайное число: " + A);
-int B = new Random().Next(1, 10);
-Console.WriteLine("Искомая степень числа: " + B);
-int degree = A;
-
-if (A == 0)
-{
-    Console.WriteLine($"Число {A} в степени {B} = 0");
-}
-else
-{
-    for (int I = 2; I < B + 1; I++)
-    {
-        degree = A * degree;
-    }
-    Console.WriteLine($"число {A} в степени {B} = {degree}");
-}*/
-
-// Решение 2.
 int DEG(int a, int b)
 {
     int deg = a;
 
 if (a == 0) return deg;
+if (b == 0) return 1;
 else
     for (int i = 2; i < b + 1; i++)
     {
@@ -34,5 +14,5 @@ else
 return deg;
 }
 int A = new Random().Next(-10, 10); Console.WriteLine("Случайное число: " + A);
-int B = new Random().Next(1, 10); Console.WriteLine("Искомая степень числа: " + B);
+int B = new Random().Next(0, 10); Console.WriteLine("Искомая степень числа: " + B);
 Console.WriteLine($"Число {A} в степени {B}: " + DEG(A, B));
