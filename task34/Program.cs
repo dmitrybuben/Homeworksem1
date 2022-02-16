3fe19611a14c195fcec4c88fda47978a0ef7515a
dmitrybuben <dmitry.buben@gmail.com>
2022-02-16 15:01:17 +0300

    task34

---
+++ task34/Program.cs
@@ -0,0 +1,30 @@
+﻿// Написать программу для замены элементов массива на противоположные.
+void FillArray(int[]array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(-10,10);
+    }
+}
+void PrintArray(int[]coll)
+{
+    for (int j = 0; j < coll.Length; j++)
+    {
+        Console.Write($"{coll[j]} ");
+    }
+    Console.WriteLine();
+}
+void PrintChangedArray(int[]collection)
+{
+    for (int n = 0; n < collection.Length; n++)
+    {
+        collection[n] = collection[n] * (-1);
+        Console.Write($"{collection[n]} ");
+    }
+    Console.WriteLine();
+}
+
+int[]num = new int[10];
+FillArray(num);
+PrintArray(num);
+PrintChangedArray(num);
