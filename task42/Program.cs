f9d035adccc95a7eefb883b07d0f11382e3a535b
dmitrybuben <dmitry.buben@gmail.com>
2022-02-19 22:58:29 +0300

    task42

---
+++ task42/Program.cs
@@ -0,0 +1,18 @@
+﻿// Определить, сколько чисел больше 0 введено с клавиатуры.
+
+int PositiveCount(string num)
+{   
+    string[]subnum = num.Split(" ");
+    int result = 0;
+    int summ = 0;
+    foreach (string item in subnum)
+    {
+        result = Convert.ToInt32(item);
+        if(result > 0) summ = summ + 1;
+    }
+    return summ;
+}
+
+Console.Write("Введите пять чисел, отделяя каждое побелом: ");
+string entries = Console.ReadLine();
+Console.WriteLine("Количество введенных чисел более 0: " + PositiveCount(entries));
