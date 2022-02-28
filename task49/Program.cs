2e56467681561eb80101f8300e4bbf82d55c0249
dmitrybuben <dmitry.buben@gmail.com>
2022-02-28 11:12:11 +0300

    task49

---
+++ task49/Program.cs
@@ -0,0 +1,18 @@
+﻿// Показать двумерный массив размером m×n, заполненный вещественными числами.
+void FillArray(double[,]matr)
+{
+    int k = new Random().Next(10, 100);
+    Console.WriteLine("Коэффициент: " + k);
+
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i,j] = Math.Round(new Random().NextDouble() * k, 3);
+            Console.Write($"{matr[i,j]} \t");
+        }
+        Console.WriteLine();
+    }
+}
+double[,] matrix = new double[3, 4];
+FillArray(matrix);
